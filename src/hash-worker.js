c668c7a894cc011ea1dd2a33feb6ab989356d07a
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2021-01-10 23:51:04 -0800

    Adding some framework for calling crypto hash functions, and testing, and benchmarking them.

--- src/hash-worker.js
+++ src/hash-worker.js
@@ -1,7 +1,12 @@
+// @ts-check
+'use strict';
+
 const {
   Worker,
   isMainThread
 } = require('worker_threads');
+const crypto = require('crypto');
+const fs = require('fs');
 
 if (isMainThread) {
   // This re-loads the current file inside a Worker instance.
@@ -10,4 +15,30 @@ if (isMainThread) {
   console.log('Inside Worker!');
   console.log(isMainThread); // Prints 'false'.
 }
+
+/**
+ * Calculate hash of file synchronously. 
+ * 
+ * @param {fs.PathLike} filename
+ * @param {any} algorithm
+ */
+const fileHash = (filename, algorithm) => {
+  const hash = crypto.createHash(algorithm || 'sha512');
+  const input = fs.createReadStream(filename);
+  let buf = {};  // TODO: Resume here! fix this so it can work!
+  input.on('readable', () => {
+    // Only one element is going to be produced by the
+    // hash stream.
+    const data = input.read();
+    if (data)
+      hash.update(data);
+    else {
+      buf = hash.digest();
+      // console.log(buf.toString('hex'));
+      // console.log(`${buf.toString('hex', 0, 1)}/${encodeURIComponent(buf.toString('base64', 1))}`);
+    }
+  });
+
+}
+
 
